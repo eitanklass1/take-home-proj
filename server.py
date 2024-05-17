07af81e73c65aea015a069a81bf4d090bb3dbfd4
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2024-05-17 17:49:58 -0500

    base template added

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def test():
   return 'Hello World'
+
+@app.route('/form')
+def form():
+  return render_template('form.html')
+
+@app.route('/form-submitted')
+def submitted_form():
+  return render_template('submitted.html')
 
 if __name__ == '__main__':
   app.run()
