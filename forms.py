57b3faa88a17500c5e10879058c6af9cf56d9487
eitanklass1 <59514595+eitanklass1@users.noreply.github.com>
2024-05-17 19:10:48 -0500

    implemented data validation

--- forms.py
+++ forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField
+from wtforms import StringField, SubmitField, EmailField, validators
 
 class LeadForm(FlaskForm):
-  first_name = StringField('First Name')
-  last_name = StringField('Last Name')
-  company_name = StringField('Company Name')
-  email = EmailField('Email')
+  first_name = StringField('First Name', [
+    validators.Length(min=2, max=25),
+    validators.InputRequired()
+  ])
+  last_name = StringField('Last Name', [
+    validators.Length(min=2, max=25),
+    validators.InputRequired()
+  ])
+  company_name = StringField('Company Name', [
+    validators.Length(min=2, max=25),
+    validators.InputRequired()
+  ])
+  email = EmailField('Email', [
+    validators.InputRequired()
+  ])
   submit = SubmitField('Submit')
